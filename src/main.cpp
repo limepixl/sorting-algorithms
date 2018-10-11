116f225b5d9410131a457b1916d3ca55f3728104
limepixl <stefs.ivanovski@gmail.com>
2018-10-11 21:50:08 +0200

    Added query for sorting algorithm to be used.

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,14 @@
 #include "algorithms/BubbleSort.h"
 #include "algorithms/InsertionSort.h"
 
+// All sorting algorithms stored in an enum
+enum SortType {
+	BUBBLE_SORT,
+	INSERTION_SORT
+};
+
 int main() {
-	int n, windowWidth, windowHeight, delay;
+	int n, windowWidth, windowHeight, delay, sortType;
 	bool sorted = false;
 
 	// Ask the user for the window's desired dimensions
@@ -21,8 +27,13 @@ int main() {
 	std::cin >> n;
 
 	// Ask the user for the desired delay between comparisons
-	std::cout << "Enter the desired delay between comparisons and draw calls, in milliseconds.\n";
+	std::cout << "Enter the delay between comparisons and draw calls, in milliseconds.\n";
 	std::cin >> delay;
+
+	// Ask the user for which algorithm to use
+	std::cout << "Enter the algorithm you would like to use.\n";
+	std::cout << "(BubbleSort = 0 | InsertionSort = 1)\n";
+	std::cin >> sortType;
 
 	// Create window
 	sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Sorting");
@@ -40,8 +51,21 @@ int main() {
 		}
 
 		if (!sorted) {
-			BubbleSort::runSort(shuffled, &window, delay);
-			sorted = true;
+			switch (sortType) {
+			case BUBBLE_SORT:
+				BubbleSort::runSort(shuffled, &window, delay);
+				sorted = true;
+				break;
+
+			case INSERTION_SORT:
+				InsertionSort::runSort(shuffled, &window, delay);
+				sorted = true;
+				break;
+
+			default:
+				std::cout << "Invalid value for sort algorithm type entered!\n";
+				return 0;
+			}
 		}
 		else {
 			window.clear(sf::Color(51, 51, 51, 255));
